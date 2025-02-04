1e8b0ddfc3b672a0d789bb5bb3d02c3c4457c988
Ahmedi7a <ahmedamralla73@email.com>
2025-02-04 13:41:03 +0300

    added more

--- controllers/applications.js
+++ controllers/applications.js
@@ -1,27 +1,40 @@
+const { application } = require('express');
 const User = require('../models/user')
 
 // page of add app
-function newApplication(req,res){
- res.render('applications/new.ejs',{title:'Add New Application'})
+function newApplication(req, res) {
+    res.render('applications/new.ejs', { title: 'Add New Application' })
 }
 
 //post the add new
-async function createApplication (req,res){
-    console.log(req.body);
-    const currentUser= await User.findById(req.params.userId)
-    currentUser.applications.push(req.body)//pushing the form data into user model
-    await currentUser.save()// it will save the push
+async function createApplication(req, res) {
+    try {
+        console.log(req.body);
+        const currentUser = await User.findById(req.params.userId)
+        currentUser.applications.push(req.body)//pushing the form data into user model
+        await currentUser.save()// it will save the push
+        res.redirect(`/users/${currentUser._id}/applications`)
+    } catch (err) {
+        console.log(err)
+        res.redirect('/');
+    }
 
-    res.redirect(`/users/${currentUser._id}/applications`)
-}   
+}
+//index page and show all apps
+ async function index(req, res) {
+    try {
+        const currentUser= await User.findById(req.params.userId);
+        res.render('applications/index.ejs', { title: 'Your Applications', applications: currentUser.applications, })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/');
+    }
 
-function index(req,res){
-    res.render('applications/index.ejs',{title:'Your Applications'})
 }
 
 
 //=================
-module.exports={
+module.exports = {
     newApplication,
     createApplication,
     index,
